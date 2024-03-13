d9630265ed5839885e4f676122d08ad9ef123502
Uday Singh <uday144@gmail.com>
2024-03-14 10:13:17 +1100

    @Named("message")  - > two NotificationService in NotificationServiceModule -> to avoid ambiguity use  @Named("message") at two places ( NotificationServiceModule and caller)

--- app/src/main/java/com/android/daggerexample/NotificationServiceModule.kt
+++ app/src/main/java/com/android/daggerexample/NotificationServiceModule.kt
@@ -2,12 +2,20 @@ package com.android.daggerexample
 
 import dagger.Module
 import dagger.Provides
+import javax.inject.Named
 
 @Module
 class NotificationServiceModule {
 
+    @Named("message")
     @Provides
     fun getMessageService() : NotificationService{
         return MessageService()
+    }
+
+    @Named("email")
+    @Provides
+    fun getEmailService(emailService: EmailService) : NotificationService{
+        return emailService
     }
 }
--- app/src/main/java/com/android/daggerexample/UserRegistrationService.kt
+++ app/src/main/java/com/android/daggerexample/UserRegistrationService.kt
@@ -1,9 +1,11 @@
 package com.android.daggerexample
 
 import javax.inject.Inject
+import javax.inject.Named
 
 class UserRegistrationService @Inject constructor(
-    private val userRepository: UserRepository, private val notificationService: NotificationService
+    private val userRepository: UserRepository,
+    @Named("message") private val notificationService: NotificationService
 ) {
 
     fun registerUser(email: String, password: String) {

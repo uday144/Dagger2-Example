e55f45075156e1f62e9f06a0d4330280dfc7e3cd
Uday Singh <uday144@gmail.com>
2024-03-14 11:31:57 +1100

    @Singleton -  emailService2: EmailService

--- app/src/main/java/com/android/daggerexample/MainActivity.kt
+++ app/src/main/java/com/android/daggerexample/MainActivity.kt
@@ -8,6 +8,14 @@ class MainActivity : AppCompatActivity() {
 
     @Inject
     lateinit var userRegistrationService: UserRegistrationService  // Field injection
+
+
+    @Inject
+    lateinit var emailService1: EmailService
+
+    @Inject
+    lateinit var emailService2: EmailService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/android/daggerexample/UserRegistrationComponent.kt
+++ app/src/main/java/com/android/daggerexample/UserRegistrationComponent.kt
@@ -2,7 +2,9 @@ package com.android.daggerexample
 
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [UserRepositoryModule::class, NotificationServiceModule::class])
 interface UserRegistrationComponent {
     fun inject(mainActivity: MainActivity) // function name doesn't matter - pass the consumer. Provided object in the consumer where @Inject on field
